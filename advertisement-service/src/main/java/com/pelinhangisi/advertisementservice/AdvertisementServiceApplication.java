4969f101e40899dde583e913faa722df938c519b
pelinhangisi <pelinhangisi@gmail.com>
2022-07-05 22:16:00 +0300

    done

--- advertisement-service/src/main/java/com/pelinhangisi/advertisementservice/AdvertisementServiceApplication.java
+++ advertisement-service/src/main/java/com/pelinhangisi/advertisementservice/AdvertisementServiceApplication.java
@@ -1,9 +1,11 @@
 package com.pelinhangisi.advertisementservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class AdvertisementServiceApplication {
 
     public static void main(String[] args) {
