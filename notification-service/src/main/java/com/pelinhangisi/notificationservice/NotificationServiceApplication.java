e66441d0dd86a78a0f79878ff5a94cc634c98c20
pelinhangisi <pelinhangisi@gmail.com>
2022-07-05 22:49:52 +0300

    done

--- notification-service/src/main/java/com/pelinhangisi/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/pelinhangisi/notificationservice/NotificationServiceApplication.java
@@ -1,9 +1,11 @@
 package com.pelinhangisi.notificationservice;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class NotificationServiceApplication {
 
     public static void main(String[] args) {
